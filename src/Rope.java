ec47df0377d2208afbc392adb545adc810206b34
mac <shahrokhhiva@gmail.com>
2022-01-19 09:23:05 +0330

    prinLeafNodes debugged satus method added

--- src/Rope.java
+++ src/Rope.java
@@ -11,18 +11,18 @@ public class Rope {
         RopeNode newRoot = new RopeNode();
         newRoot.left = root;
         newRoot.right = nptr;
-        newRoot.value = newRoot.left.value ;
+        newRoot.value = newRoot.left.value;
         if (newRoot.left.right != null)
             newRoot.value += newRoot.left.right.value;
         root = newRoot;
     }
 
-    public void add(String newString){
-        while (newString!=null){
+    public void add(String newString) {
+        while (newString != null) {
             if (newString.contains(" ")) {
-                structRoop(newString.substring(0, newString.indexOf(' '))+'_');
+                structRoop(newString.substring(0, newString.indexOf(' ')) + '_');
                 newString = newString.substring(newString.indexOf(' ') + 1, newString.length());
-            }else {
+            } else {
                 structRoop(newString.substring(0, newString.length()));
                 break;
             }
@@ -31,41 +31,50 @@ public class Rope {
     }
 
 
-    public static char index(RopeNode node,int i){
-        if (node.value <= i && node.right!=null){
+    public static char index(RopeNode node, int i) {
+        if (node.value <= i && node.right != null) {
             return index(node.right, i - node.value);
         }
-        if (node.left!=null) {
+        if (node.left != null) {
             return index(node.left, i);
         }
         return node.data.charAt(i);
     }
 
 
-    //bug full
-    void printLeafNodes(RopeNode root) {
+    static void printLeafNodes(RopeNode root) {
 
-        if (root.left == null && root.right == null) {
-//            System.out.print(root.data);
-//            return;
-//        }
-        if (root.left != null){
-            printLeafNodes(root.left);
+        if (root == null)
+            return;
+
+        if (root.left == null &&
+                root.right == null)
+        {
+            System.out.print(root.data);
+            return;
         }
-        if (root.right != null){
-            return printLeafNodes(root.right);
+        if (root.left != null&&root.right != null){
+            printLeafNodes(root.left);}
+
+        if (root.right != null&&root.left != null){
+            printLeafNodes(root.right);}
+    }
+    public static void status(){
+        for (int i = 0; i < ropes.size(); i++) {
+            printLeafNodes(ropes.get(i));
+            System.out.println("\n");
         }
+
     }
 
+        public static void main (String[]args){
+            Rope rope = new Rope();
+            rope.add("hiva");
+            rope.add("shahrokh");
+            System.out.println(ropes.get(1).value);
+            System.out.println(ropes.get(1).data);
 
-    public static void main(String[] args) {
-        Rope rope = new Rope();
-        rope.add("hiva");
-        rope.add("shahrokh");
-        System.out.println(ropes.get(1).value);
-        System.out.println(ropes.get(1).data);
 
+        }
 
     }
-
-}
