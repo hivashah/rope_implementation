076d8abc814f50d6d377dba544441c947e8c127f
MEHRAN <mehran.sfi16@gmail.com>
2022-01-22 01:21:19 +0330

    A new recursive method for searching between our ropeNodes and finding appropriate, and a concat method is added!

--- src/Rope.java
+++ src/Rope.java
@@ -59,6 +59,8 @@ public class Rope {
         if (root.right != null&&root.left != null){
             printLeafNodes(root.right);}
     }
+
+
     public static void status(){
         for (int i = 0; i < ropes.size(); i++) {
             printLeafNodes(ropes.get(i));
@@ -67,12 +69,37 @@ public class Rope {
 
     }
 
+
+    public static RopeNode concat(RopeNode ropeNode1 , RopeNode ropeNode2){
+        RopeNode newRoot = new RopeNode();
+        newRoot.value = ropeNode1.value;
+        newRoot.left = ropeNode1;
+        newRoot.right = ropeNode2;
+        return newRoot;
+    }
+
+    public static RopeNode search(RopeNode node,int i){
+        if (node.value < i && node.right!=null){
+            return search(node.right, i - node.value);
+        }
+        if (node.left!=null) {
+            return search(node.left, i);
+        }
+        return node;
+    }
+
         public static void main (String[]args){
             Rope rope = new Rope();
-            rope.add("hiva");
-            rope.add("shahrokh");
-            System.out.println(ropes.get(1).value);
-            System.out.println(ropes.get(1).data);
+            rope.add("Hiva");
+            Rope rope1 = new Rope();
+            rope1.add("Mehran");
+            System.out.println(concat(rope.root,rope1.root).data);
+
+            System.out.println(index(rope.root , 5));
+            System.out.println(search(rope.root,5));
+
+//            System.out.println(ropes.get(1).value);
+//            System.out.println(ropes.get(1).data);
 
 
         }
